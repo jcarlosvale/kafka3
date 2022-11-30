39e562d641d23045af576c689a6e6cf4ef233f27
jcarlos.vale <jcarlos.vale@gmail.com>
2022-11-30 16:24:51 +0000

    kafka idempotent

---
+++ kafka-producer-wikimedia/src/main/java/io/demos/kafka/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,48 @@
+package io.demos.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements EventHandler {
+
+    private final KafkaProducer<String, String> kafkaProducer;
+    private final String topic;
+
+    private static final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class.getSimpleName());
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> kafkaProducer, String topic) {
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() {
+        //nothing to do
+    }
+
+    @Override
+    public void onClosed() {
+        kafkaProducer.close();
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) {
+        log.info(messageEvent.getData());
+        //async code
+        kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String s) {
+        //nothing to do
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        log.error("Error reading the stream", throwable);
+    }
+}
